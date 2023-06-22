f0e93d6566734057c050ca279bbc07a1b9741b28
Timur <johnsyplay@yandex.ru>
2023-06-22 16:48:29 +0300

    fixed

--- store/jwt.slice.ts
+++ store/jwt.slice.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { getCookie, setCookie } from "cookies-next";
-import { HYDRATE } from "next-redux-wrapper";
 import { AppState } from ".";
 
 export interface state {
